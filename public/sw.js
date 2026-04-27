fddeecc7cc928e27809e4c6369f2434e672c8c01
BrenoH. <brenosc60@gmail.com>
2026-04-27 01:49:28 -0300

    feat: adiciona suporte a PWA com manifest e service worker

---
+++ public/sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'coup-v1';
+const STATIC = [
+  '/',
+  '/css/style.css',
+  '/js/index.js',
+  '/js/game.js',
+  '/js/lobby.js',
+  '/manifest.json',
+  '/images/icon.svg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // Nunca intercepta Socket.IO
+  if (url.pathname.startsWith('/socket.io')) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        if (res.ok && e.request.method === 'GET') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('/')))
+  );
+});
